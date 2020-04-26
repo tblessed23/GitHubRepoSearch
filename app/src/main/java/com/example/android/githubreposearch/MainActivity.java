4b46855cc2af48fbf0741cf6c43d00b17e637614
Theonna Stacy <msmbsolutions@gmail.com>
2020-04-26 11:33:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/githubreposearch/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.githubreposearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText mSearchBoxEditText;
+    TextView mUrlDisplayTextView;
+    TextView mSearchResultsTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+
+
+    }
+}
