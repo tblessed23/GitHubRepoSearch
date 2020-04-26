ca78e7f6fedc72117d134d162b47c4872b2c1886
Theonna Stacy <msmbsolutions@gmail.com>
2020-04-26 12:40:56 -0500

    Up To >> T02.02-Exercise-AddMenu

--- app/src/main/java/com/example/android/githubreposearch/MainActivity.java
+++ app/src/main/java/com/example/android/githubreposearch/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.android.githubreposearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,36 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
+
+    }
+
+    //Display or Menu; See Menus form Android Developer console
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int menuItemThatWasSelected = item.getItemId();
+
+        if  (menuItemThatWasSelected == R.id.action_search){
+
+            Context context = getApplicationContext();
+            CharSequence text = "Search clicked";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
 
     }
 }
